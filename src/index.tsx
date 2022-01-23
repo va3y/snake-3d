3d1256046fc1f6cc37c4daeb101f3451fa1ec9d3
Valeriy Ivanov <valerih333@gmail.com>
2022-01-23 14:20:23 +0300

    change router to hash

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App";
 import StartScreen from "./pages/StartScreen";
 import GameScreen from "./pages/GameScreen";
@@ -8,14 +8,14 @@ import "./assets/css/index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<App />}>
           <Route path="/" element={<StartScreen />} />
           <Route path="game" element={<GameScreen />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
